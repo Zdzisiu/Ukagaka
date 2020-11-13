e346c6205743df72c867e9fc98f62c66e1bf7158
Zdzisiu <ola2013.2013@interia.pl>
2020-11-13 23:26:12 +0100

    site

---
+++ js.js
@@ -0,0 +1,15 @@
+var colors = ["#ff89ef", "#89ffff", "#92ff89", "#faff89", "#ff8989", "#9591ff"];
+
+var random_color = colors[Math.floor( 
+                    Math.random() * colors.length)]; 
+
+document.getElementById("style1").style.color = random_color;
+document.getElementById("style2").style.color = random_color;
+document.getElementById("style3").style.color = random_color;
+document.getElementById("style4").style.color = random_color;
+document.getElementById("style5").style.color = random_color;
+document.getElementById("style6").style.color = random_color;
+document.getElementById("style7").style.color = random_color;
+document.getElementById("style8").style.color = random_color;
+document.getElementById("style9").style.color = random_color;
+document.getElementById("style10").style.color = random_color;
